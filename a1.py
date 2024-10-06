ce6a5c19e73d446ac698a9f9428585262fe4eac1
Zainasyed14 <zainasyed12345@gmail.com>
2024-10-06 05:12:54 -0700

    first commit

---
+++ a1.py
@@ -0,0 +1,6 @@
+num = int(input("Enter numerator : "))
+mun = int(input("Enter denominator : "))
+if num%mun==0 :
+    print(num , "is divisible by" , mun)
+else:
+    print(mun , "is not divisible by", num)
---
+++ main.py
@@ -0,0 +1,12 @@
+one = float(input("Enter the speed : "))
+two = float(input("Enter the speed : "))
+three = float(input("Enter the speed : "))
+sum = one+two+three
+avg= sum/3
+print(avg)
+if one<avg  :
+    print("one is slower than the average speed")
+elif two<avg :
+    print("two is slower than the average speed")
+elif three<avg :
+    print("three is slower than the average speed")
